98a8b3ced0d6bb0b183f2991a2b5510785805d8b
francovinicius <duartevini.dev@gmail.com>
2023-09-10 12:49:31 -0300

    first commit

---
+++ app/src/main/java/com/simpletask/sorteioapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.simpletask.sorteioapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
